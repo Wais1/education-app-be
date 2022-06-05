18576fb61e16e62c7ab21690c6b454b01ec6acb4
Wais Ibrahim <waisibrahim77@gmail.com>
2022-06-06 01:04:16 +0700

    updated for nodeflux api backend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors') // To prevent CORS errors
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const axios = require('axios')
 
 // Connect to database
 connectDB()
@@ -22,6 +23,55 @@ app.use(cors())
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/content', require('./routes/contentRoutes'))
+
+// Nodeflux auth
+//error happening during sending data
+app.post('/nodefluxauth', (req, res) => {
+    // Send auth req with axios here
+    axios.post('https://backend.cloud.nodeflux.io/auth/signatures', {
+        "access_key": "I3EQ8R022FGOKEKLEYS61T5YE",
+        "secret_key": "QhfnDPj0E1C2llbhMszWF4iBt0kJpivQF_Fbk80rRmDz2KIQsuvxrL9S0ifNiCRq"
+      }).then((response) => {
+          console.log(response.data)
+          res.send(response.data)
+      }).catch((error) => {
+        console.log(error)
+        res.send(error)
+    })
+});
+
+// Nodeflux facemask
+app.post('/nodefluxfacemask', (req, res) => {
+    console.log('hit!!!')
+    const config = req.body.config
+    const images = req.body.images
+
+    // Send facemask req with axios here
+    // Sends array of images (length 1)
+    axios.post('https://api.cloud.nodeflux.io/v1/analytics/face-mask', {
+        "images": images
+    }, config).then((response) => {
+        console.log(response.data)
+        res.send(response.data)
+    }).catch(error => {
+        console.log(error)
+        res.send(error)
+    })
+// console.log(req.body.config)
+//     console.log(req.body.images)
+
+});
+
+// Nodeflux facemask polling
+app.post('/nodefluxcheckjob', (req, res) => {
+    const config = req.body.config
+    const jobId = req.body.jobId
+    
+    console.log('checking job!! hit')
+    axios.get(`https://api.cloud.nodeflux.io/v1/jobs/${jobId}`, config).then((response) => { 
+        res.send(response.data) 
+    })
+})
 
 // Override the default Express error handler with our own
 // Shows error stack if in dev, else do not show in production
